90c7a2156c3404e6e009be7c5d976b9f88b473b7
Kenneth Fisher <kenandcrys@live.com>
2023-06-14 15:50:42 -0500

    1550

--- backend/db/models/booking.js
+++ backend/db/models/booking.js
@@ -1,27 +1,38 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+
+const { Model } = require('sequelize');
+
 module.exports = (sequelize, DataTypes) => {
   class Booking extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
       // define association here
     }
-  };
-  Booking.init({
-    id: DataTypes.INTEGER,
-    spotId: DataTypes.INTEGER,
-    userId: DataTypes.INTEGER,
-    startDate: DataTypes.DATEONLY,
-    endDate: DataTypes.DATEONLY
-  }, {
-    sequelize,
-    modelName: 'Booking',
-  });
+  }
+
+  Booking.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true, // Add this line to mark 'id' as the primary key
+        autoIncrement: true,
+      },
+      spotId: DataTypes.INTEGER,
+      userId: DataTypes.INTEGER,
+      startDate: DataTypes.DATEONLY,
+      endDate: DataTypes.DATEONLY,
+    },
+    {
+      sequelize,
+      modelName: 'Booking',
+    }
+  );
+
   return Booking;
 };
+
+
+
+
+
+
+
