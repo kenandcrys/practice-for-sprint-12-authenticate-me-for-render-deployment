338919bd7dea86b5d603bdaf6d84650e7951d1c7
Kenneth Fisher <kenandcrys@live.com>
2023-06-15 16:01:56 -0500

    1401

--- backend/psql-setup-script.js
+++ backend/psql-setup-script.js
@@ -1,5 +1,7 @@
 const { sequelize } = require('./db/models');
 
+const url = process.env.DATABASE_URL;
+
 sequelize.showAllSchemas({ logging: false }).then(async (data) => {
   if (!data.includes(process.env.SCHEMA)) {
     await sequelize.createSchema(process.env.SCHEMA);
